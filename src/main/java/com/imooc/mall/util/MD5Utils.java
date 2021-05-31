f0a593273b2b8d78130192ce2bdef92c91a2e277
wuxiaoran <wuxiaoran@xinzhili.cn>
2021-05-31 13:51:43 +0800

    save

--- src/main/java/com/imooc/mall/util/MD5Utils.java
+++ src/main/java/com/imooc/mall/util/MD5Utils.java
@@ -13,7 +13,7 @@ import java.security.NoSuchAlgorithmException;
 public class MD5Utils {
   public static String getMD5String(String strValue) throws NoSuchAlgorithmException {
     MessageDigest md5 = MessageDigest.getInstance("MD5");
-    return Base64.encodeBase64String(md5.digest(strValue.getBytes(StandardCharsets.UTF_8)));
+    return Base64.encodeBase64String(md5.digest((strValue + Constant.SALT).getBytes(StandardCharsets.UTF_8)));
   }
   // 测试生成的md5的值
   public static void main(String[] args) {
